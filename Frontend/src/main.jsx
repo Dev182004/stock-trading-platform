51e3be2da7082e729e52aa1830ecbc5f947c5015
Divyansh Tak <divyanshtak2004@gmail.com>
2026-06-28 12:44:38 +0530

    completed hashrouter migration

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,11 +11,11 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
         <Toaster position="top-right" />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
